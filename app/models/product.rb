c151fc684f9dc2ae9232aba33c3913212d04be9e
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-02-26 13:58:42 +0300

    user done

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,12 @@ class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
 
-    def leave_review(user, star_rating, comment)
-
+    def leave_review(user:, star_rating:, comment:)
+        @user = user
+        @star_rating = star_rating
+        @comment = comment
+        user_instance = self.users.find_by(name: @user)
+        self.reviews.create(star_rating: @star_rating, comment: @comment, user_id: user_instance.id, product_id: self.id)
     end
 
     def print_all_reviews
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
     has_many :products, through: :reviews
 
     def favorite_product
-        rev=self.reviews.map{|r| r.star_rating}.max
-        rev1 = self.reviews.find_by(star_rating: rev)
+        rev_star_rating=self.reviews.map{|r| r.star_rating}.max
+        rev1 = self.reviews.find_by(star_rating: rev_star_rating)
         self.products.find(rev1.product_id)
     end
 
