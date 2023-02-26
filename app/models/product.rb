4eb3d9cc236495e5da09d4fb4e32ef7c60c5f580
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-02-26 12:59:23 +0300

    product done

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+
+    end
+
+    def print_all_reviews
+        self.reviews.map do |r|
+            puts "Review for #{self.name} by #{self.users.find(r.user_id).name}: #{r.star_rating}. #{r.comment}"
+        end
+    end
+
+    def average_rating
+        rating = self.reviews.map {|r| r.star_rating}
+        rating.sum(0.0) / rating.size
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
+
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    def favorite_product
+        rev=self.reviews.map{|r| r.star_rating}.max
+        rev1 = self.reviews.find_by(star_rating: rev)
+        self.products.find(rev1.product_id)
+    end
 end
